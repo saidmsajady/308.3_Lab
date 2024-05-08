9df2328c98177ebcb509329a3b667084e8cd087d
Said Masih <saidmsajady@gmail.com>
2024-05-07 20:17:47 -0700

    Finished, whew

--- script.mjs
+++ script.mjs
@@ -18,45 +18,68 @@ for (let i = 1; i <= 100; i++) {
 
 console.log(`\nPart Two`);
 
-let n = 11;
+let ogN = 8
+let n = ogN;
 let isPrime = true;
 
-for (let i = 2; i < n; i++) {
-    if (n % i == 0) {
-        isPrime = false;
+for (let i = 2; ; n++) {
+    isPrime = true;
+    for (let j = 2; j <= n / 2; j++) {
+        if (n % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        console.log(`The next prime number after ${ogN} is ${n}`);
         break;
     }
-}
-
-if (isPrime) {
-    console.log(`${n} is a prime number`);
 }
 
 // Part Three = Feeling Loopy
 
 console.log(`\nPart Three`);
 
-// const str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+const string = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-// const rows = str.split('\n');
-// const headers = rows[0].split(',');
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+let currentCell = ""; 
 
-// let cell1, cell2, cell3, cell4;
+for (const char of string) {
+    if (char === ',') {
+        // When you encounter a comma, move to the next cell.
+        if (cell1 === "") {
+            cell1 = currentCell;
+        } else if (cell2 === "") {
+            cell2 = currentCell;
+        } else if (cell3 === "") {
+            cell3 = currentCell;
+        } else if (cell4 === "") {
+            cell4 = currentCell;
+        }
+        currentCell = "";
+    } else if (char === '\n') {
+        // When you encounter the “\r\n” sequence, move to the next “row.”
+        cell4 = currentCell;
+        console.log(cell1, cell2, cell3, cell4);
 
-// for (const row of rows.slice(1)) { // Start from index 1 to skip the header row
-//     const cells = row.split(',');
-//     if (cells[0] === "42" || cells[0] === "57" || cells[0] === "63" || cells[0] === "98") {
-//         cell1 = cells[0];
-//     }
-//     if (cells[1] === "Bruce" || cells[1] === "Bob" || cells[1] === "Blaine" || cells[1] === "Bill") {
-//         cell2 = cells[1];
-//     }
-//     if (cells[2] === "Knight" || cells[2] === "Fry Cook" || cells[2] === "Quiz Master" || cells[2] === "Doctor’s Assistant") {
-//         cell3 = cells[2];
-//     }
-//     if (cells[3] === "41" || cells[3] === "19" || cells[3] === "58" || cells[3] === "26") {
-//         cell4 = cells[3];
-//     }
-// }
+        // Reset cells for next row
+        cell1 = "";
+        cell2 = "";
+        cell3 = "";
+        cell4 = "";
+        currentCell = "";
+    } else {
+        // Append character to current cell
+        currentCell += char;
+    }
+}
 
-// console.log(cell1, cell2, cell3, cell4);
+// Log the last row
+if (currentCell !== "") {
+    cell4 = currentCell;
+    console.log(cell1, cell2, cell3, cell4);
+}
